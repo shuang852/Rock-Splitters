60e62f1d5e608301ec7f369d71286d3495d0b4f7
Jordan Hamlin <Jordan.T.Hamlin@gmail.com>
2022-01-16 12:05:07 +1100

    Created main menu scene and updated UI Framwork package.

---
+++ Assets/Scripts/UI/Generic/LoadSceneButton.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using UI.Core;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace UI.Generic
+{
+    [RequireComponent(typeof(Button))]
+    public class LoadSceneButton : DialogueComponent<Dialogue>
+    {
+        [SerializeField] private int sceneIndexInBuild;
+        
+        private Button button;
+
+        private static Coroutine currentCoroutine;
+        
+        protected override void OnComponentAwake()
+        {
+            TryGetComponent(out button);
+            button.onClick.AddListener(OnSubmit);
+        }
+        
+        protected override void Subscribe() { }
+        
+        protected override void Unsubscribe() { }
+
+        private void OnSubmit()
+        {
+            if (currentCoroutine != null)
+            {
+                Debug.LogError("Tried loading scene while another was already being loaded!");
+                return;
+            }
+                
+            Scene scene = SceneManager.GetSceneByBuildIndex(sceneIndexInBuild);
+            Debug.Log($"Loading Scene '{scene.name}'.");
+            currentCoroutine = StartCoroutine(LoadSceneAsync(scene));
+        }
+
+        private IEnumerator LoadSceneAsync(Scene scene)
+        {
+            AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndexInBuild);
+            while (!operation.isDone)
+                yield return null;
+
+            currentCoroutine = null;
+        }
+    }
+}
