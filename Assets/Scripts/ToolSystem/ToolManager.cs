abd893e752078a4adab81d4786bb652f2ccd4c0a
Jack Coggins <voidawake90@gmail.com>
2022-01-17 12:59:58 +1100

    Tool System
    Added ToolManager, Tool scriptable objects and basic hammer and drill tools. Switch tools by pressing [Q].
    
    Added functions to ChunkManager, some of which might need to be made more efficient. Would also be good to come back and disambiguate the different chunk coordinate systems.

---
+++ Assets/Scripts/ToolSystem/ToolManager.cs
@@ -0,0 +1,70 @@
+using System.Collections.Generic;
+using System.Linq;
+using Managers;
+using RockSystem;
+using UnityEngine;
+
+namespace ToolSystem
+{
+    public class ToolManager : Manager
+    {
+        [SerializeField] private List<Tool> tools;
+
+        private ChunkManager chunkManager;
+
+        private int currentToolIndex = 0;
+
+        private Tool CurrentTool => tools.ElementAtOrDefault(currentToolIndex);
+
+
+        protected override void Start()
+        {
+            base.Start();
+            
+            chunkManager = M.GetOrThrow<ChunkManager>();
+        }
+
+        /// <summary>
+        /// Called once when the tool starts being used. Similar to <c>Input.GetKeyDown</c>.
+        /// </summary>
+        public void ToolDown(Vector2 worldPosition)
+        {
+            if (CurrentTool.action == Tool.ToolAction.Tap)
+                UseTool(worldPosition);
+        }
+
+        /// <summary>
+        /// Called each frame the tool is being used including the first. Similar to <c>Input.GetKey</c>.
+        /// </summary>
+        /// <param name="worldPosition"></param>
+        public void ToolInUse(Vector2 worldPosition)
+        {
+            if (CurrentTool.action == Tool.ToolAction.Continuous)
+                UseTool(worldPosition);
+        }
+
+        /// <summary>
+        /// Called once when the tool stops being used. Similar to <c>Input.GetKeyUp</c>.
+        /// </summary>
+        public void ToolUp(Vector2 worldPosition) {}
+
+        private void UseTool(Vector2 worldPosition)
+        {
+            List<Vector2Int> affectedChunks = chunkManager.GetChunksInRadius(worldPosition, CurrentTool.radius);
+
+            foreach (var affectedChunk in affectedChunks)
+            {
+                chunkManager.DamageChunk(affectedChunk, CurrentTool.damage);
+            }
+        }
+
+        // TODO: Temporary. Remove once tool selection UI is added.
+        public void CycleTools()
+        {
+            if (++currentToolIndex >= tools.Count)
+                currentToolIndex = 0;
+            
+            Debug.Log($"Switched to {CurrentTool.name}");
+        }
+    }
+}
