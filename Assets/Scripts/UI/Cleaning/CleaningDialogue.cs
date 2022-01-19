de1db663dbcb8735604d408fb10a515651641aa5
Jack Coggins <voidawake90@gmail.com>
2022-01-19 14:02:40 +1100

    Start cleaning button
    Created cleaning dialogue and button to start cleaning.

---
+++ Assets/Scripts/UI/Cleaning/CleaningDialogue.cs
@@ -0,0 +1,13 @@
+﻿using UI.Core;
+
+namespace UI.Cleaning
+{
+    public class CleaningDialogue : Dialogue
+    {
+        protected override void OnClose() { }
+
+        protected override void OnPromote() { }
+
+        protected override void OnDemote() { }
+    }
+}
