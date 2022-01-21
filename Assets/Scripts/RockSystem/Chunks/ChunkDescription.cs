f66fa0300e4943bce51e658da097352f7c34f4a9
Jack Coggins <voidawake90@gmail.com>
2022-01-21 14:26:42 +1100

    Chunk health variation
    Also tweaked layer numbers, chunk health, and tool damages

--- Assets/Scripts/RockSystem/Chunks/ChunkDescription.cs
+++ Assets/Scripts/RockSystem/Chunks/ChunkDescription.cs
@@ -9,9 +9,12 @@ namespace RockSystem.Chunks
         [SerializeField] private Vector2 offset;
         [SerializeField] private Color damagedColor = Color.clear;
         [SerializeField] private int health;
+        [Tooltip("A random number is generated between -X and X and added to the base chunk health.")]
+        [SerializeField] private int healthVariation;
 
         public Vector2 Offset => offset;
         public Sprite Sprite => sprite;
         public int Health => health;
+        public int HealthVariation => healthVariation;
     }
 }
