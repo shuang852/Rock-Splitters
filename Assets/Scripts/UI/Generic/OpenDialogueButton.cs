0e7aafa616f5daf919a06e3b3d80f940a96b48a4
Jordan Hamlin <Jordan.T.Hamlin@gmail.com>
2022-01-30 18:16:00 +1100

    Added some generic UI components and created pause menu (in progress).

---
+++ Assets/Scripts/UI/Generic/OpenDialogueButton.cs
@@ -0,0 +1,19 @@
+﻿using UI.Core;
+using UI.Generic;
+using UnityEngine;
+
+namespace UI.Pause
+{
+    public class OpenDialogueButton : DialogueButton<Dialogue>
+    {
+        [SerializeField] private GameObject dialoguePrefab;
+
+        protected override void Subscribe() { }
+        protected override void Unsubscribe() { }
+
+        protected override void OnClick()
+        {
+            Instantiate(dialoguePrefab);
+        }
+    }
+}
