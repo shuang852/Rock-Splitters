1ef0a524215d58d33c104dcf8a0b8b92e3c8bb6f
Sam <sam.huang852@gmail.com>
2022-02-07 14:37:25 +1100

    Awaiting change to cleaning loop

---
+++ Assets/Scripts/UI/Cleaning/CleaningScoreUI.cs
@@ -0,0 +1,35 @@
+﻿using System.Globalization;
+using Cleaning;
+using Managers;
+using TMPro;
+using UI.Core;
+
+namespace UI.Cleaning
+{
+    public class CleaningScoreUI : DialogueComponent<CleaningDialogue>
+    {
+        private CleaningScoreManager manager;
+        private TextMeshProUGUI text;
+
+        protected override void OnComponentAwake()
+        {
+            TryGetComponent(out text);
+        }
+
+        protected override void OnComponentStart()
+        {
+            base.OnComponentStart();
+            manager = M.GetOrThrow<CleaningScoreManager>();
+        }
+
+        protected override void Subscribe() { }
+
+        protected override void Unsubscribe() { }
+
+        // TODO: Turn this to event based
+        private void Update()
+        {
+            text.text = manager.Score.ToString(CultureInfo.InvariantCulture);
+        }
+    }
+}
