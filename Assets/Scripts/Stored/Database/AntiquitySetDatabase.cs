af62a9c9e2108befede0f8ce056bf4a46c9db454
Sam <sam.huang852@gmail.com>
2022-02-08 13:58:48 +1100

    Persistent manager spawner done and fixed a few fossil assets

---
+++ Assets/Scripts/Stored/Database/AntiquitySetDatabase.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Object = UnityEngine.Object;
+#if UNITY_EDITOR
+using System;
+using UnityEditor;
+#endif
+
+namespace Stored
+{
+    [CreateAssetMenu(menuName = "SO/DB/AntiquitySetDB")]
+    public class AntiquitySetDatabase : AssetDatabase<AntiquitySet>
+    {
+        public AntiquitySet[] Items => items;
+        public bool overrideOrder;
+        [SerializeField] private AntiquitySet[] orderedItems;
+        public AntiquitySet[] OrderedItems => orderedItems;
+        
+#if UNITY_EDITOR
+        private void OnValidate()
+        {
+            LoadItems();
+            if (!overrideOrder)
+            {
+                CreateOrderedList();
+            }
+        }
+
+        [ContextMenu("Create List")]
+        private void CreateOrderedList()
+        {
+            orderedItems = items.OrderBy(set => set.ProdID).ToArray();
+            // TODO: Verify there are no dupes. Output error if there is.
+            // var dupes = orderedItems.GroupBy(x => x.ProdID)
+            //     .Where(g => g.Count() > 1)
+            //     .Select(y => new { Element = y.Key, Counter = y.Count()})
+            //     .ToArray();
+            //
+            // if (dupes.Length <= 0) return;
+            // foreach (var dupe in dupes)
+            // {
+            //     Debug.LogError($"{dupe.Element}");
+            // }
+        }
+#endif
+    }
+}
---
+++ Assets/Scripts/Stored/Database/PersistentManagersDB.cs
@@ -0,0 +1,15 @@
+﻿using Managers;
+using UnityEditor;
+using UnityEngine;
+
+namespace Stored
+{
+    [CreateAssetMenu(fileName = "PersistentManagersDB", menuName = "SO/DB/PersistentManagersDB", order = 0)]
+    public class PersistentManagersDB : AssetDatabase<GameObject>
+    {
+        public GameObject[] Items => items;
+#if UNITY_EDITOR
+        private void OnValidate() => LoadItems();
+#endif
+    }
+}
