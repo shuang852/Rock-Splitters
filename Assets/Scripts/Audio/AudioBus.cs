2f265047fa2d3762c007dc174b5cdec30f60632e
Jack Coggins <voidawake90@gmail.com>
2022-03-10 21:41:42 +1100

    Refactor AudioManager

---
+++ Assets/Scripts/Audio/AudioBus.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+namespace Audio
+{
+    [Serializable]
+    public class AudioBus
+    {
+        public Bus enumValue;
+
+        public float Volume
+        {
+            get => volume;
+            set
+            {
+                volume = value;
+                bus.setVolume(volume);
+            }
+        }
+
+        [SerializeField] private string path;
+        
+        private FMOD.Studio.Bus bus;
+        private float volume;
+
+        public void Initialise()
+        {
+            bus = FMODUnity.RuntimeManager.GetBus(path);
+
+            Volume = 1;
+        }
+    }
+}
